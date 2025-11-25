fa8ddcdbe6aa431f6fd7cd4766ad3a3e22a12154
guesung <gueit214@naver.com>
2025-11-25 10:19:36 +0900

    refactor: 중복된 export 제거

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,7 +5,7 @@ import { ModalBody } from './ModalBody';
 import { ModalFooter } from './ModalFooter';
 import { ModalCloseButton } from './ModalCloseButton';
 
-export const Modal = Object.assign(ModalRoot, {
+const Modal = Object.assign(ModalRoot, {
   Content: ModalContent,
   Header: ModalHeader,
   Body: ModalBody,
